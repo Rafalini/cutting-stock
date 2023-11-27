0caaf95283f3f9a8ffa3e3e2bab54ba2b17c1350
rafalini <prajmik@gmail.com>
2023-11-27 12:33:51 +0100

    addons

--- implementation/src/InputGenerator.py
+++ implementation/src/InputGenerator.py
@@ -2,6 +2,7 @@ import json
 import math
 import random
 import os, sys
+from unittest import result
 
 def clearInputsOutputs():
 
@@ -39,13 +40,37 @@ def generateData(relaxation=False, max_percentage=50, factory_rod_size=15, order
 
     return {"factory_rod_size": factory_rod_size, "order": order}
 
+def reverseGenerator(relaxation=False, max_percentage=50, factory_rod_size=15, order_size=10):
+    order={}
+
+    for i in range(order_size):
+
+        remaining_length = factory_rod_size
+
+        while remaining_length > 0:
+            # Generate a random number between 1 and the remaining sum
+            random_num = random.randint(1, remaining_length)
+            
+            # If the remaining sum is greater than the random number, append the random number
+            if remaining_sum > random_num:
+                order[random_num] = order.get(random_num, 0) + 1
+                remaining_sum -= random_num
+            else:
+                # If the remaining sum is less than or equal to the random number, append the remaining sum
+                order[random_num] = order.get(remaining_sum, 0) + 1
+                break
+
+
+
+    return {"factory_rod_size": factory_rod_size, "order": order}
+
 if __name__ == "__main__":
     clearInputsOutputs()
     for i in range(1, int(sys.argv[1])+1):
         name = str(i) + ".json"
         with open(os.path.join("input", name), "w") as outfile:
             if len(sys.argv) == 0:
-                data = generateData(relaxation=True, max_percentage = 20, factory_rod_size = 20, order_size=5 + i)
+                data = generateData(relaxation=True, max_percentage = 20, factory_rod_size = 12, order_size=5 + i)
             else:
-                data = generateData(relaxation=True, max_percentage = 20, factory_rod_size = 20, order_size=5 + i)
+                data = generateData(relaxation=True, max_percentage = 20, factory_rod_size = 12, order_size=5 + i)
             outfile.write(json.dumps(data))
